56b16be7465bb5eb9f7c2de34438a23e92717cfa
yanagita1007 <maachan1510@gmail.com>
2024-09-27 12:15:04 +0900

    web.php修正、composerインストールできない

--- week40-1/src/routes/web.php
+++ week40-1/src/routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
-Route::get('/login', 'Auth\LoginController@login');
+Route::get('/login', 'Auth\LoginController@showLoginForm');
+//Route::get('/login', 'Auth\LoginController@login');
 
 
