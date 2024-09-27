71a5d9d7466751d789eb590cc853b41f86b9c5c8
yanagita1007 <maachan1510@gmail.com>
2024-09-27 14:23:20 +0900

    40-1終了

--- week40-1/src/routes/web.php
+++ week40-1/src/routes/web.php
@@ -15,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index');
 
 Route::get('/login', 'Auth\LoginController@showLoginForm');
-//Route::get('/login', 'Auth\LoginController@login');
 
 
