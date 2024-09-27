f9a04aa76e51949bcec3372aa7c94141007a2ce9
yanagita1007 <maachan1510@gmail.com>
2024-09-27 13:23:36 +0900

    43-1終了

--- week43-1/src/resources/views/quiz/create.blade.php
+++ week43-1/src/resources/views/quiz/create.blade.php
@@ -11,7 +11,7 @@
                 </ul>
             </div>
         @endif
-        <form action="{{ route('quizzes.store') }}" method="post">
+        <form action="{{ route('quizzes.store') }}" method="post" enctype="multipart/form-data">
             @csrf
 
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="file">Upload
